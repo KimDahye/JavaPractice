dd02703d4627f5b15f7e37fcf096320ed96e8a0e
Dahye Kim <dahye.kim@nhnnext.org>
2014-07-13 21:53:09 +0900

    Scanner

--- StringCalculator/src/StringCalculator.java
+++ StringCalculator/src/StringCalculator.java
@@ -1,22 +1,36 @@
+import java.util.Scanner;
+
 public class StringCalculator {
 
-	public static void main(String [] as){
-		//String nums = "3,4\n5";
-		String nums = "";
-		
-		String [] numbers = nums.split("[,\\n]");
-		int sum = 0;
-		for(int i = 0; i < numbers.length; i++)
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		String quit = "";
+		do
 		{
-			if(numbers[i].equals(""))
+			System.out.println("Input text: ");
+			String nums = sc.nextLine();
+			
+			String [] numbers = nums.split(",|\\\\n");
+			int sum = 0;
+			for(int i = 0; i < numbers.length; i++)
 			{
-				break;
+				if(numbers[i].equals(""))
+				{
+					break;
+				}
+				
+				sum = sum + Integer.parseInt(numbers[i]);
 			}
 			
-			sum = sum + Integer.parseInt(numbers[i]);
-		}
+			System.out.println("The sum is "+sum);
+			
+			System.out.println("Enter 'q' if you want to quit");
+			quit = sc.nextLine(); 
+			
+		}while(!quit.equals("q"));
+		sc.close();
 		
-		System.out.println("The sum is "+sum);
+		
 	}
 	
 }
