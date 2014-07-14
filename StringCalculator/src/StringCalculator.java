c596ebffba0f2372e7a848fbe7790e600cb8e13b
Dahye Kim <dahye.kim@nhnnext.org>
2014-07-14 16:03:52 +0900

    refactoring2

--- StringCalculator/src/StringCalculator.java
+++ StringCalculator/src/StringCalculator.java
@@ -7,8 +7,8 @@ public class StringCalculator {
 		String quit = "";
 		
 		do{
-			String [] numbers = inputNumbers(sc);
-			addNumbers(numbers);		
+			String [] values = inputNumbers(sc);
+			add(values);		
 			quit = ask(sc);	 
 			
 		}while(!quit.equals("q"));
@@ -23,19 +23,33 @@ public class StringCalculator {
 		return sc.nextLine().split(",|\\\\n");
 	}
 	
-	public static void addNumbers(String [] numbers){
-		int sum = 0;
+	public static void add(String [] values){
 		
-		if(numbers[0].equals("")){
-			System.out.println("The sum is "+sum);
+		if(values[0].equals("")){
+			System.out.println("The sum is 0.");
 			return;
 		}
 		
-		for(int i = 0; i < numbers.length; i++){
-			sum = sum + Integer.parseInt(numbers[i]);
-		}
-		System.out.println("The sum is "+sum);
+		int [] numbers = toInt(values); 
+		System.out.println("The sum is "+ sum(numbers));
 		
+	}
+	
+	public static int [] toInt(String [] values)
+	{
+		int [] result = new int [values.length];
+		for(int i = 0; i < values.length; i++){
+			result [i] = Integer.parseInt(values[i]);
+		}
+		return result;
+	}
+	
+	public static int sum(int [] numbers){
+		int sum = 0;
+		for(int i = 0; i < numbers.length; i++){
+			sum = sum+ numbers [i]; 
+		}
+		return sum;
 	}
 	
 	public static String ask(Scanner sc)
