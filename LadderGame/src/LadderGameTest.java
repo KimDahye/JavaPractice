6802a7c7b4d205248cfa2cb3377155189886d82c
Dahye Kim <dahye.kim@nhnnext.org>
2014-07-23 10:42:41 +0900

    LadderGame bug

--- LadderGame/src/LadderGameTest.java
+++ LadderGame/src/LadderGameTest.java
@@ -1,12 +1,23 @@
 import junit.framework.TestCase;
 
 public class LadderGameTest extends TestCase {
-
-	public void testLadderPrint() {
+	Ladder ladder;
+	
+	public void setUp(){
 		int peopleNum = 6;
 		int height = 10;
-		Ladder ladder = new Ladder(peopleNum, height);
+		ladder = new Ladder(peopleNum, height);
+	}
+
+	public void testLadderPrint() {
 		ladder.print();
+	}
+	
+	public void testMinimi(){
+		Minimi m1 = new Minimi(1); 
+		m1.run(ladder);
+		assertEquals(6,m1.printResult());
+	
 	}
 	
 }
--- LadderGame/src/Minimi.java
+++ LadderGame/src/Minimi.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Minimi {
-	ArrayList<Node> path;
+	ArrayList<Node> path = new ArrayList<Node> ();
 	int startingPos;
 	Node presentPos;
 
@@ -11,12 +11,20 @@ public class Minimi {
 		path.add(presentPos);
 		
 	}
+	
+	public int printResult()
+	{
+		int result;
+		result = (presentPos.getCol() + 1)/2;
+		System.out.println("The result: " + result);
+		return result;
+	}
 
 	public void run(Ladder ladder) {
 		while(!ladder.isEnd(presentPos)){
 			if(ladder.isCrossable(presentPos) && !isPassed())
 			{
-				cross();
+				cross(presentPos);
 				continue;
 				  
 			}
@@ -25,9 +33,23 @@ public class Minimi {
 		}
 	}
 	
-	private void cross(){
+	private void cross(Node presentPos){
 		//미니미 현재 포지션 왼쪽으로 두칸, 혹은 오른쪽으로 두칸 바꿔줌
 		//path에 경로 더하기
+		if(left().getRow() == 1)
+		{
+			presentPos = left();
+			path.add(presentPos);
+			presentPos = left();
+			path.add(presentPos);
+			return;
+		}
+		
+		presentPos = right();
+		path.add(presentPos);
+		presentPos = right();
+		path.add(presentPos);
+		
 	}
 	
 	private void moveForward(){
